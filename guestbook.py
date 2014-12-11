3556198780813c42b446ec82dff1160da4deebea
Genevieve <glinchan@gmail.com>
2014-12-10 22:48:31 -0800

    summary

--- guestbook.py
+++ guestbook.py
@@ -31,7 +31,6 @@ class QuestionResponse(ndb.Model):
 class SurveyResponse(ndb.Model):
     """Models an individual Guestbook entry with author, content, and date."""
     author = ndb.UserProperty()
-    content = ndb.StringProperty(indexed=False)
     date = ndb.DateTimeProperty(auto_now_add=True)
     question_responses = ndb.StructuredProperty(QuestionResponse, repeated=True)    
 
@@ -44,6 +43,7 @@ class ResultsPage(webapp2.RequestHandler):
             ancestor=guestbook_key(guestbook_name)).order(-SurveyResponse.date)
         survey_responses = surveys_query.fetch(10)
 
+        #login
         if users.get_current_user():
             url = users.create_logout_url(self.request.uri)
             url_linktext = 'Logout'
@@ -51,19 +51,36 @@ class ResultsPage(webapp2.RequestHandler):
             url = users.create_login_url(self.request.uri)
             url_linktext = 'Login'
 
-        QUESTION_NAME = 'Best dog question'
-        result_summary = {
-          QUESTION_NAME: {}
-        }
+
+        #aggregates question responses
+        result_summary = {}
+
+        # loop through each responder's survey responses to aggregate # of "x" response
+
         for survey_response in survey_responses:
-            # get this person's response to question 0
-            favorite_resp = survey_response.question_responses[0].response
-            # initialize our counter for this response if we have to
-            if favorite_resp not in result_summary[QUESTION_NAME]:
-                result_summary[QUESTION_NAME][favorite_resp] = 0
-            # increment the counter for this response
-            result_summary[QUESTION_NAME][favorite_resp] += 1
-        # end for
+            print "result summary " + str(result_summary)
+            for question_response in survey_response.question_responses:
+                question_summary={}
+                if question_response.question + " summary" in result_summary:
+                    question_summary = result_summary[question_response.question+ ' summary']
+                print "question_summary= " + str(question_summary)
+                if question_response.response:
+                    if question_response.response not in question_summary:
+                        question_summary[question_response.response]=0
+                    question_summary[question_response.response]+=1
+                result_summary[question_response.question +' summary']=question_summary
+
+
+
+
+        #     # get this person's response to question 0
+        #     favorite_resp = survey_response.question_responses[0].response
+        #     # initialize our counter for this response if we have to
+        #     if favorite_resp not in result_summary[QUESTION_NAME]:
+        #         result_summary[QUESTION_NAME][favorite_resp] = 0
+        #     # increment the counter for this response
+        #     result_summary[QUESTION_NAME][favorite_resp] += 1
+        # # end for
 
         total_responses = len(survey_responses)
         template_values = {
@@ -72,7 +89,6 @@ class ResultsPage(webapp2.RequestHandler):
             'survey_responses': survey_responses,
             'url': url,
             'url_linktext': url_linktext,
-            'Frank':"woof"
         }
 
         template = JINJA_ENVIRONMENT.get_template('results.html')
@@ -102,7 +118,7 @@ class MainPage(webapp2.RequestHandler):
         }
 
         template = JINJA_ENVIRONMENT.get_template('index.html')
-        self.response.write(template.render(template_values))
+        self.response.write(template.render(template_values)) 
 
 
 class SurveyResponseHandler(webapp2.RequestHandler):
@@ -119,13 +135,52 @@ class SurveyResponseHandler(webapp2.RequestHandler):
         if users.get_current_user():
             survey_response.author = users.get_current_user()
 
+        
+
+        print self.request.get_all("breed")
         survey_response.question_responses = [
+          
+          #comment box
           QuestionResponse(
             question='Best Dogs Response',
             response=self.request.get('comment')),
+          
+          #radio button
           QuestionResponse(
             question='sex response',
-            response=self.request.get('sex'))]
+            response=self.request.get('sex')),
+          
+          #dropdown
+          QuestionResponse(
+            question='corgi name dropdown response',
+            response=self.request.get('corgi_name')),
+          
+
+
+          #checkbox
+          QuestionResponse(
+            question='checkbox breed response',
+            response=self.request.get('breed1')),
+          QuestionResponse(
+            question='checkbox breed response',
+            response=self.request.get('breed2')),
+          QuestionResponse(
+            question='checkbox breed response',
+            response=self.request.get('breed3')),
+
+          #multiple textboxes
+          QuestionResponse(
+            question='city response',
+            response=self.request.get('city')),
+          QuestionResponse(
+            question='state response',
+            response=self.request.get('state')),
+          QuestionResponse(
+            question='county response',
+            response=self.request.get('county'))
+
+
+          ]
 
 
         # to add a second question
